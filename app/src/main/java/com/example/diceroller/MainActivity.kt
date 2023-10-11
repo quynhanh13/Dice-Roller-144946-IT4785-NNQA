992281676be98ffbb2ecac6847c30efaa86d2a2b
quynhanh13 <quynhanh1392002@gmail.com>
2023-10-11 22:04:57 +0700

    two dices roller

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceImage: ImageView = findViewById(R.id.imageView2)
         val drawableResource = when (diceRoll) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -29,12 +29,28 @@ class MainActivity : AppCompatActivity() {
         }
 
         diceImage.setImageResource(drawableResource)
-        diceImage.contentDescription = diceRoll.toString()
+
+        val dice2 = Dice(6)
+        val diceRoll2 = dice2.roll2()
+        val diceImage2: ImageView = findViewById(R.id.imageView)
+        val drawableResource2 = when (diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage2.setImageResource(drawableResource2)
     }
 }
 class Dice(private val numSides: Int) {
 
     fun roll(): Int {
+        return (1..numSides).random()
+    }
+    fun roll2(): Int {
         return (1..numSides).random()
     }
 }
